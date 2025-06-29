0a3eb436a763a0abd439c12c2432d52796f1f6f2
Kostiantyn7714 <77kot1976@gmail.com>
2025-06-29 15:00:12 +0200

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,5 @@
+
+document.getElementById('feedback-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Ваше сообщение отправлено! (в демонстрационной версии)');
+});
